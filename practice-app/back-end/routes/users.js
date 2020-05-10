0ebcd263970415275c783f3ab349d4b66ea3c42d
barisbasmak <barisbasmak@gmail.com>
2020-05-10 14:48:07 +0300

    registration without encryption done

---
+++ practice-app/back-end/routes/users.js
@@ -0,0 +1,28 @@
+const { User, validate } = require('../models/user');
+const express = require('express');
+const router = express.Router();
+ 
+router.post('/', async (req, res) => {
+    // First Validate The Request
+    const { error } = validate(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+ 
+    // Check if this user already exisits
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+        return res.status(400).send('That user already exisits!');
+    } else {
+        // Insert the new user if they do not exist yet
+        user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        });
+        await user.save();
+        res.send(user);
+    }
+});
+ 
+module.exports = router;

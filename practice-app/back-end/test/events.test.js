9f747e5a7732bdf4e7e29200fcf1ffb1dcca2a99
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-05-27 17:43:30 +0300

    tests for events/search route and related service methods

--- practice-app/back-end/test/events.test.js
+++ practice-app/back-end/test/events.test.js
@@ -1,4 +1,6 @@
 import app from '../src/index';
+import { getConferences } from '../src/services/eventService';
+import { getRelatedKeywords } from '../src/services/relatedKeywords';
 
 const request = require('supertest');
 const { expect } = require('chai');
@@ -45,5 +47,85 @@ describe('/GET geoLocation service with place parameter', () => {
     const response = await getLocation('Istanbul');
     const result = response.split(' ').map(Number);
     expect(result).to.deep.equal([41.009998, 28.950001]);
+  });
+});
+
+describe('eventService / getRelatedKeywords method', () => {
+  it('should respond with a list of keywords for a valid keyword', async () => {
+    const response = await getRelatedKeywords('test', 10);
+    expect(response).to.be.an('array');
+    expect(response).to.have.length.lessThan(12);
+  });
+
+  it('should respond with an empty list for empty string', async () => {
+    const response = await getRelatedKeywords('', 10);
+    expect(response).to.be.an('array');
+    expect(response).to.have.length(1);
+  });
+});
+
+describe('eventService / getConferences method', () => {
+  it('should respond with a list of conferences for a valid keyword list', async () => {
+    const response = await getConferences(['machine learning', 'ai']);
+    expect(response).to.have.property('count').to.be.a('number');
+    expect(response).to.have.property('results').to.be.an('array');
+    const { results } = response;
+    results.forEach((result) => {
+      expect(result).to.have.property('title');
+      expect(result).to.have.property('description');
+      expect(result).to.have.property('labels');
+      expect(result).to.have.property('start');
+      expect(result).to.have.property('end');
+      expect(result).to.have.property('timezone');
+      expect(result).to.have.property('location');
+      expect(result).to.have.property('scope');
+      expect(result).to.have.property('country');
+    });
+  });
+
+  it('should respond with an empty list of conferences for an empty list', async () => {
+    const response = await getConferences([]);
+    expect(response).to.have.property('count').to.be.a('number').to.equal(0);
+    expect(response).to.have.property('results').to.be.an('array').to.have.length(0);
+  });
+});
+
+describe('GET  events/search', () => {
+  it('should respond with a list of conferences for a valid key', async () => {
+    const response = await request(app)
+      .get('/api/events/search?key=machine learning')
+      .expect(200);
+    expect(response.body.count).to.be.a('number').to.be.greaterThan(0);
+    expect(response.body.results).to.be.an('array').to.have.length.greaterThan(0);
+    response.body.results.forEach((result) => {
+      expect(result).to.have.property('title');
+      expect(result).to.have.property('description');
+      expect(result).to.have.property('labels');
+      expect(result).to.have.property('start');
+      expect(result).to.have.property('end');
+      expect(result).to.have.property('timezone');
+      expect(result).to.have.property('location');
+      expect(result).to.have.property('scope');
+      expect(result).to.have.property('country');
+    });
+  });
+
+  it('should respond with a list of conferences for an empty key', async () => {
+    const response = await request(app)
+      .get('/api/events/search')
+      .expect(200);
+    expect(response.body.count).to.be.a('number').to.be.greaterThan(0);
+    expect(response.body.results).to.be.an('array').to.have.length.greaterThan(0);
+    response.body.results.forEach((result) => {
+      expect(result).to.have.property('title');
+      expect(result).to.have.property('description');
+      expect(result).to.have.property('labels');
+      expect(result).to.have.property('start');
+      expect(result).to.have.property('end');
+      expect(result).to.have.property('timezone');
+      expect(result).to.have.property('location');
+      expect(result).to.have.property('scope');
+      expect(result).to.have.property('country');
+    });
   });
 });

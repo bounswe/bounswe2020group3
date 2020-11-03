0daf30e63032a0c84715c967a1cd40e041533628
Ahmet Emir <37846781+liberbey@users.noreply.github.com>
2020-11-03 15:24:17 +0300

    pylint removed

--- backend/setup.sh
+++ backend/setup.sh
@@ -10,7 +10,6 @@ echo "finished to create virtual environment and install packages"
 
 # copy pre-commit to git hooks
 cp pre-commit ../.git/hooks
-cp .pylintrc ../.git/hooks
 echo "Copied pre-commit file to hooks"
 
 exit 0

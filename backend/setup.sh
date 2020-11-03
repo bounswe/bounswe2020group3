45a9a68395a4e45ceea1878ce3d76182f8a0ab7b
Ahmet Emir <37846781+liberbey@users.noreply.github.com>
2020-11-03 12:48:38 +0300

    setup.sh

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,3 +1,13 @@
+# prepare virtual environment and install packages
+echo "starting to create virtual environment and install packages"
+py -m venv orgenv
+
+source orgenv/Scripts/activate
+
+pip install -r requirements.txt
+echo "finished to create virtual environment and install packages"
+
+
 # copy pre-commit to git hooks
 cp pre-commit .git/hooks
 cp .pylintrc .git/hooks

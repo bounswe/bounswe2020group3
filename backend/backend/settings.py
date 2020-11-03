ba9eea2254a9c253b6a93f703bfe9daa4961084b
Ahmet Emir <37846781+liberbey@users.noreply.github.com>
2020-11-03 14:09:12 +0300

    pylint check

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+# comment to check pylint
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.

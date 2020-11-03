448a0f1bb3ea3860fb3cb51374887374a11a54fa
Ahmet Emir <37846781+liberbey@users.noreply.github.com>
2020-11-03 14:27:52 +0300

    pylint and bandit check

--- backend/setup.sh
+++ backend/setup.sh
@@ -9,8 +9,8 @@ echo "finished to create virtual environment and install packages"
 
 
 # copy pre-commit to git hooks
-cp pre-commit .git/hooks
-cp .pylintrc .git/hooks
+cp pre-commit ../.git/hooks
+cp .pylintrc ../.git/hooks
 echo "Copied pre-commit file to hooks"
 
 exit 0

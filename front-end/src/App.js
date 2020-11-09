9aa735fdcd33b2e779e2db86c4eff3e006f29405
Mert Tahran <44242500+merttahran@users.noreply.github.com>
2020-11-09 20:19:37 +0300

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
+import RegistrationPage from "./Views/RegistrationPage";
+import HomePage from "./Views/HomePage";
+
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Switch>
-            <Route exact path="/" component={} />
-            <Route path="/home" component={} />
-            <Route path="/login" component={} />
-            <Route path="/registration" component={} />
-            <Route path="/paperapi" component={} />
+            <Route exact path="/" component={HomePage } />
+            <Route path="/home" component={HomePage} />
+            <Route path="/login" component={RegistrationPage} />
+            <Route path="/registration" component={RegistrationPage} />
+            <Route path="/paperapi" component={RegistrationPage} />
           </Switch>
     </div>
   );
 }
-
 export default App;

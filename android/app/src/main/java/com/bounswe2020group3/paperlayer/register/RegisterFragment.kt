8aa224892de2559bd527536fb439a9f27e57345c
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-11-10 13:56:30 +0300

    Move RegisterFragment.kt under register folder

--- android/app/src/main/java/com/bounswe2020group3/paperlayer/register/RegisterFragment.kt
+++ android/app/src/main/java/com/bounswe2020group3/paperlayer/register/RegisterFragment.kt
@@ -1,4 +1,4 @@
-package com.bounswe2020group3.paperlayer
+package com.bounswe2020group3.paperlayer.register
 
 
 import android.os.Bundle
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bounswe2020group3.paperlayer.R
 
 class RegisterFragment : Fragment() {
 

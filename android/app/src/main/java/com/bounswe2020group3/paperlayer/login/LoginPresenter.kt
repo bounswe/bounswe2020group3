9ed10a97db79bbc189d230df536c6d1f35a3a0b4
rambo250 <ramazankoc5252@gmail.com>
2020-11-11 00:24:17 +0300

    LoginPresenter.kt: Updated according to reviews.

--- android/app/src/main/java/com/bounswe2020group3/paperlayer/login/LoginPresenter.kt
+++ android/app/src/main/java/com/bounswe2020group3/paperlayer/login/LoginPresenter.kt
@@ -3,17 +3,18 @@ package com.bounswe2020group3.paperlayer.login
 import android.widget.EditText
 import androidx.navigation.Navigation
 import com.bounswe2020group3.paperlayer.R
+import kotlinx.android.synthetic.main.fragment_login.view.*
 
 class LoginPresenter: LoginContract.Presenter {
 
-    private lateinit var mView: LoginContract.View
+    private lateinit var view: LoginContract.View
 
     override fun onViewCreated() {
         TODO("Not yet implemented")
     }
 
     override fun setView(view: LoginContract.View) {
-        this.mView =view
+        this.view =view
     }
 
     override fun showToast(message: String) {
@@ -21,9 +22,7 @@ class LoginPresenter: LoginContract.Presenter {
     }
 
     override fun created() {
-        this.mView.bindViews()
-        this.mView.initOnClicks()
-
+        this.view.initOnClicks()
     }
 
     override fun onLoginButtonClicked(mailEditText: EditText, passwordEditText: EditText) {
@@ -31,21 +30,20 @@ class LoginPresenter: LoginContract.Presenter {
         val userPassword=passwordEditText.text.toString()
         //Model is not implemented yet
         //Ex: AuthenticationSystem.check(userEmail,userPassword)
-        mailEditText.text.clear()
-        passwordEditText.text.clear()
+        view.resetEditText()
         //Navigation must be changed to profile page after profile page created
-        Navigation.findNavController(mView.getLayout()).navigate(R.id.navigateToRegister)
+        Navigation.findNavController(view.getLayout()).navigate(R.id.navigateToRegister)
     }
 
     override fun onRegisterButtonClicked(mailEditText: EditText, passwordEditText: EditText) {
-        mailEditText.text.clear()
-        passwordEditText.text.clear()
-        Navigation.findNavController(mView.getLayout()).navigate(R.id.navigateToRegister)
+        view.resetEditText()
+        Navigation.findNavController(view.getLayout()).navigate(R.id.navigateToRegister)
     }
 
     override fun onGuestButtonClicked(mailEditText: EditText, passwordEditText: EditText) {
+        view.resetEditText()
         //Navigation must be changed to guest page after guest page created
-        Navigation.findNavController(mView.getLayout()).navigate(R.id.navigateToRegister)
+        Navigation.findNavController(view.getLayout()).navigate(R.id.navigateToRegister)
     }
 
 }

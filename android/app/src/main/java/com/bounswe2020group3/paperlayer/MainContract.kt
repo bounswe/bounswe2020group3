7b9ba3cb4e046af7eb5de1eaef455d3d7478efd4
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-11-10 13:43:41 +0300

    Move base presenter and view under base folder

--- android/app/src/main/java/com/bounswe2020group3/paperlayer/MainContract.kt
+++ android/app/src/main/java/com/bounswe2020group3/paperlayer/MainContract.kt
@@ -1,5 +1,8 @@
 package com.bounswe2020group3.paperlayer
 
+import com.bounswe2020group3.paperlayer.base.BasePresenter
+import com.bounswe2020group3.paperlayer.base.BaseView
+
 interface MainContract {
   interface Presenter : BasePresenter {
     fun onViewCreated()

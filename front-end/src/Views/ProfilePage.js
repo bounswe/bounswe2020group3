d3aa8d26501a901a2146b52325b446e277c75e33
Mert Tahran <44242500+merttahran@users.noreply.github.com>
2020-11-10 14:58:22 +0300

    Update ProfilePage.js

--- front-end/src/Views/ProfilePage.js
+++ front-end/src/Views/ProfilePage.js
@@ -77,7 +77,6 @@ export default class HomePage extends Component {
     };
 
     render() {
-      // console.log(this.props.history, "asd")
         if (!this.state.success) {
             return (
                 <Container>

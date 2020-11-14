2863a9df19c3d0e042646544c3109b5c92c7a5c8
Ahmet Emir <37846781+liberbey@users.noreply.github.com>
2020-11-14 16:30:25 +0300

    project initial

--- backend/api/views/project.py
+++ backend/api/views/project.py
@@ -1,22 +1,20 @@
 from requests import Response
 from rest_framework import viewsets
 from rest_framework import permissions
-from rest_framework.decorators import action
-from rest_framework import renderers
-from api.models.profile import Profile
-from api.permission import IsOwnerOrReadOnly
+from api.models.project import Project
+from api.permission import IsMemberOrReadOnly
 from api.serializers.profile import ProfileSerializer
 
 
-class ProfileViewSet(viewsets.ModelViewSet):
+class ProjectViewSet(viewsets.ModelViewSet):
     """
     This viewset automatically provides `list`, `create`, `retrieve`,
     `update` and `destroy` actions.
     """
-    queryset = Profile.objects.all()
+    queryset = Project.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly]
+                          IsMemberOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)

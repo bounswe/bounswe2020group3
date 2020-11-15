a9729f896f1793653dec78db1ce2e1fb4f8d9a0e
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-11-15 21:55:49 +0300

    Create Profile data class

---
+++ android/app/src/main/java/com/bounswe2020group3/paperlayer/profile/data/Gender.kt
@@ -0,0 +1,7 @@
+package com.bounswe2020group3.paperlayer.profile.data
+
+enum class Gender(var detail: String) {
+    MALE("male"),
+    FEMALE("female"),
+    NOT_SPECIFIED("do not want to share")
+}

f44c9ffb400696fbd4980d00235d6c30102e2d76
barisbasmak <barisbasmak@gmail.com>
2020-11-17 17:57:36 +0300

    test removed, browser router bug fixed

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,14 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import history from "./history";
 
 ReactDOM.render(
-    <BrowserRouter history={history}>
-        <App/>
-    </BrowserRouter>,
+        <App/>,
     document.getElementById("root")
 );
-

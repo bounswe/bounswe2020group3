db53c0ebcbe67c2e92e8ddc7fce94440bd6c4cfb
barisbasmak <barisbasmak@gmail.com>
2020-11-17 17:57:36 +0300

    test removed, browser router bug fixed

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,22 +2,25 @@ import './App.css';
 import RegistrationPage from "./Views/RegistrationPage";
 import HomePage from "./Views/HomePage";
 import LoginPage from "./Views/LoginPage";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 import config from "./config";
-import { AuthenticateBeforeRender, AuthenticatedRoute } from './Components/Auth/Authenticate';
+import { AuthenticatedRoute } from './Components/Auth/Authenticate';
+import history from "./history";
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-
-            <Route exact path="/" component={HomePage } />
-            <Route path="/home" component={HomePage} />
-            <Route path={config.Login_Path} component={LoginPage} />
-            <Route path={config.Register_Path} component={RegistrationPage} />
+      <BrowserRouter history={history}>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/home" component={HomePage} />
+          <Route path={config.Login_Path} component={LoginPage} />
+          <Route path={config.Register_Path} component={RegistrationPage} />
           {/* Left as an example for constructing an authenticated route */}
-            <AuthenticatedRoute path="/paperapi" component={LoginPage} />
-      </Switch>
+          <AuthenticatedRoute path="/paperapi" component={LoginPage} />
+        </Switch>
+      </BrowserRouter>
+
     </div>
   );
 }

f8f41d48959544318806ea306e6d7946ad3dc807
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-11-17 11:47:14 +0300

    Implement profile fetch

--- android/app/src/main/java/com/bounswe2020group3/paperlayer/profile/ProfileModel.kt
+++ android/app/src/main/java/com/bounswe2020group3/paperlayer/profile/ProfileModel.kt
@@ -1,4 +1,19 @@
 package com.bounswe2020group3.paperlayer.profile
 
+import com.bounswe2020group3.paperlayer.profile.data.Profile
+import com.bounswe2020group3.paperlayer.network.RetrofitProvider
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
 class ProfileModel: ProfileContract.Model {
+
+    private var profileService: ProfileContract.ProfileService = RetrofitProvider.instance.create(ProfileContract.ProfileService::class.java)
+
+    override fun getProfile(profileId: Int): Single<Profile> {
+        return profileService.getProfile(profileId)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+    }
+
 }

209560c1384cdb318581a16e83eb343c0c762640
rambo250 <ramazankoc5252@gmail.com>
2020-11-20 18:35:04 +0300

    ProjectCard.kt: Data class for project

--- android/app/src/main/java/com/bounswe2020group3/paperlayer/project/ProjectCard.kt
+++ android/app/src/main/java/com/bounswe2020group3/paperlayer/project/ProjectCard.kt
@@ -3,7 +3,8 @@ package com.bounswe2020group3.paperlayer.project
 data class ProjectCard(
     var projectTitle: String,
     var projectBody: String,
-    var projectCreator: String
+    var projectCreator: String,
+    var projectId: Int
 ) {
 
 

665106eff6fa467ca42962b5fe72378c13a5785b
Ali Furkan Budak <40214943+alifurkanbudak@users.noreply.github.com>
2020-11-21 18:15:43 +0300

    Simplified events field to django's default

--- backend/api/serializers/project.py
+++ backend/api/serializers/project.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from api.models.project import Project
-from api.serializers.event import EventSerializer
 
 
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
@@ -8,10 +7,9 @@ class ProjectSerializer(serializers.HyperlinkedModelSerializer):
     Project serializer, includes also profile owner
     """
     owner = serializers.ReadOnlyField(source='owner.username')
-    events = EventSerializer(many=True)
 
     class Meta:
         model = Project
-        fields = ['name', 'description', 'requirements', 'owner',
+        fields = ['id', 'name', 'description', 'requirements', 'owner',
                   'members', 'is_public', 'state', 'project_type', 'due_date',
                   'events']

4d6172a036884f46b16da61b396ac48d118d75dc
Ali Furkan Budak <40214943+alifurkanbudak@users.noreply.github.com>
2020-11-22 15:10:53 +0300

    Added 'event_type' filter

--- backend/api/views/event.py
+++ backend/api/views/event.py
@@ -2,6 +2,7 @@ from api.models.event import Event
 from rest_framework import viewsets
 from rest_framework import permissions
 from api.serializers.event import EventSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class EventViewSet(viewsets.ModelViewSet):
@@ -12,3 +13,5 @@ class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['event_type']

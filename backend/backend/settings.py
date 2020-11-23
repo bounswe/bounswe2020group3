e623ce460d887ba8dd373940d96fce34e924cd28
Furkan Cansever <furkancansever@Furkans-MacBook-Air.local>
2020-11-23 17:05:58 +0300

    Dockerfile configuration changed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,12 +86,24 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get('DJANGO_ENV', 'development') != 'production':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('DB_NAME', 'paperlayer'),
+            'USER': os.environ.get('DB_USER', 'paperlayer'),
+            'PASSWORD': os.environ.get('DB_PASSWORD', 'paperlayer'),
+            'HOST': os.environ.get('DB_ADDRESS', '127.0.0.1'),
+            'PORT': os.environ.get('DB_PORT', 5432)
+        }
+    }
 
 SWAGGER_SETTINGS = {
     'SECURITY_DEFINITIONS': {
--- backend/docker/run-local.sh
+++ backend/docker/run-local.sh
@@ -6,4 +6,4 @@ echo "Migrate database..."
 python manage.py migrate
 
 echo "Start debug server with live reload"
-exec python manage.py runserver 0.0.0.0:8080
+exec python manage.py runserver 0.0.0.0:80

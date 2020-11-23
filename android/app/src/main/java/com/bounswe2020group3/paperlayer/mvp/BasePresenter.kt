c6d120519d864e4b267a077f89ec734e23b25d0f
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-11-23 11:41:07 +0300

    BasePresenter.kt: Add CallSuper annotations
    Signed-off-by: Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>

--- android/app/src/main/java/com/bounswe2020group3/paperlayer/mvp/BasePresenter.kt
+++ android/app/src/main/java/com/bounswe2020group3/paperlayer/mvp/BasePresenter.kt
@@ -1,13 +1,17 @@
 package com.bounswe2020group3.paperlayer.mvp
 
+import androidx.annotation.CallSuper
+
 abstract class BasePresenter<V: Mvp.View>  : Mvp.Presenter<V> {
 
     protected var view: V? = null
 
+    @CallSuper
     override fun bind(view: V) {
         this.view = view
     }
 
+    @CallSuper
     override fun unbind() {
         this.view = null
     }

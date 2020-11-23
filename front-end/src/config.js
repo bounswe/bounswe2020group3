1c9ad73745c95a5482d64fcb679ad156681be9cd
Mert Tahran <44242500+merttahran@users.noreply.github.com>
2020-11-23 11:44:30 +0300

    Update config.js

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,8 +1,15 @@
-export default {
-  //API_URL: process.env.REACT_APP_API_URL,
-  API_URL: "http://localhost:8000", //for convenience
+const config = {
+  API_URL: process.env.REACT_APP_API_URL,
   Login_Path : "/login",
   Register_Path: "/register",
   Login_Url : "/api/auth/",
-  Register_Url : "/api/register/"
+  Register_Url : "/api/register/",
+  Event_Creation_Path : "/create-event",
+  Event_Creation_Url : "/api/events/",
+  Create_Project_Path :"/create-project",
+  Create_Project_Url : "/api/projects/",
+  Projectpage_Path: "/project",
+  Projectpage_url: "/api/projects/",
+  Homepage_Path: "/"
 }
+export default config;

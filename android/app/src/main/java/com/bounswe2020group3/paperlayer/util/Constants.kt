2d5cc41d217e2333cfe74b8ca7a0a1c93721c240
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-11-24 22:27:40 +0300

    Constants.kt: Add AWS url
    Signed-off-by: Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>

--- android/app/src/main/java/com/bounswe2020group3/paperlayer/util/Constants.kt
+++ android/app/src/main/java/com/bounswe2020group3/paperlayer/util/Constants.kt
@@ -1,4 +1,5 @@
 package com.bounswe2020group3.paperlayer.util
 
 const val BASE_URL = "https://paperlayer.herokuapp.com"
+//const val BASE_URL = "http://ec2co-ecsel-1egsefcb8a7i3-34132836.us-east-2.elb.amazonaws.com"
 const val TOKEN = "Token 94636236e607c560cb13333e4b60b9faa5bc125d"

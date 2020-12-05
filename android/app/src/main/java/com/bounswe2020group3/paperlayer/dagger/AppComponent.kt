c5125e78f6f6e8d2ea863fae817e1e9ed7afd6f5
mhrfky <44057069+mhrfky@users.noreply.github.com>
2020-12-05 15:44:36 +0300

    dagger implementation done

--- android/app/src/main/java/com/bounswe2020group3/paperlayer/dagger/AppComponent.kt
+++ android/app/src/main/java/com/bounswe2020group3/paperlayer/dagger/AppComponent.kt
@@ -6,10 +6,11 @@ import com.bounswe2020group3.paperlayer.profile.edit.ProfileEditFragment
 import com.bounswe2020group3.paperlayer.project.ProjectFragment
 import com.bounswe2020group3.paperlayer.project.ProjectMainFragment
 import com.bounswe2020group3.paperlayer.projectCreate.ProjectCreateFragment
+import com.bounswe2020group3.paperlayer.register.RegisterFragment
 import dagger.Component
 import javax.inject.Singleton
 
-@Component(modules = [NetworkModule::class, ProfileModule::class, LoginModule::class,ProjectModule::class, SessionModule::class, ProjectCreateModule::class])
+@Component(modules = [RegisterModule::class, NetworkModule::class, ProfileModule::class, LoginModule::class,ProjectModule::class, SessionModule::class, ProjectCreateModule::class])
 @Singleton
 interface AppComponent {
     fun inject(fragment: ProfileFragment)
@@ -18,4 +19,6 @@ interface AppComponent {
     fun inject(fragment: ProjectMainFragment)
     fun inject(fragment: ProjectFragment)
     fun inject(fragment: ProjectCreateFragment)
+    fun inject(fragment: RegisterFragment)
+
 }

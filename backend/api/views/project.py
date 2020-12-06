a6f76dbf8d45abefd2596d7236f161ef2e2fe5ca
Ali Furkan Budak <40214943+alifurkanbudak@users.noreply.github.com>
2020-12-06 15:15:36 +0300

    Fixing project endpoint structure
    - Events field in Project model changed to "event" and now returns one object or null not a list.
    - Project object now accepts ID for event and ID list for tags, in requests other than GET
    - GET request to "projects" endpoint now returns data of linked objects (events, tags, members, milestones) instead of URLs

--- backend/api/views/project.py
+++ backend/api/views/project.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from api.models.project import Project
 from api.permission import IsMemberOrReadOnly
-from api.serializers.project import ProjectSerializer
+from api.serializers.project import ProjectSerializer, ProjectGETSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 
 
@@ -20,3 +20,9 @@ class ProjectViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return ProjectGETSerializer
+
+        return super().get_serializer_class()

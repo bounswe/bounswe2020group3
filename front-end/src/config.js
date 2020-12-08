1f1114f59d36f355909aeb2bcb5bdcfdbf06cde0
emirhanyasin <e.yasincetin@gmail.com>
2020-12-08 14:09:39 +0300

    editing profile page enabled

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: process.env.REACT_APP_API_URL,
+  API_URL: "http://paperlayer.herokuapp.com",
   Login_Path : "/login",
   Register_Path: "/register",
   Login_Url : "/api/auth/",

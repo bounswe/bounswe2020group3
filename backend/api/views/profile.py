54e04d711fcf140dd7b5e9028a3fbdcad21bd79d
adilnumancelik2017400198 <44122592+adilnumancelik2017400198@users.noreply.github.com>
2020-12-08 18:57:50 +0300

    Added profile_picture to profile
    -Added profile_picture field to profile
    -Added retrieve_profile_picture endpoint
    -Added affiliations field to profile
    -Removed photo_url field from profile
    -Added Pillow to poetry  (used for images)

--- backend/api/views/profile.py
+++ backend/api/views/profile.py
@@ -4,6 +4,9 @@ from api.models.profile import Profile
 from api.permission import IsOwnerOrReadOnly
 from api.serializers.profile import ProfileSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import action
+from django.http import FileResponse
+from rest_framework.parsers import MultiPartParser, FormParser
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
@@ -11,12 +14,18 @@ class ProfileViewSet(viewsets.ModelViewSet):
     This viewset automatically provides `list`, `create`, `retrieve`,
     `update` and `destroy` actions.
     """
+    parser_classes = (MultiPartParser, FormParser)
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['owner__id']
+
+    @action(detail=True)
+    def retrieve_profile_picture(self, request, pk=None):
+        picture = self.get_object().profile_picture
+        return FileResponse(picture)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)

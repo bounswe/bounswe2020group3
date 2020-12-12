37afcec1a154f0fa06352ec22a79b47742e82a33
Mert Tahran <44242500+merttahran@users.noreply.github.com>
2020-12-12 15:43:48 +0300

    Update ProjectPage.js
    updating project page layout

--- front-end/src/Views/ProjectPage.js
+++ front-end/src/Views/ProjectPage.js
@@ -11,20 +11,24 @@ import SvgIcon from '@material-ui/core/SvgIcon';
 import Typography from "@material-ui/core/Typography";
 
 const Container = styled(Box)({
-    background: "white",
-    border: 0,
-    borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    height: "100vh",
-    width: "100%",
-    margin: "auto",
-    '& .MuiTextField-root': {
-        margin: "10px",
-        width: "30%",
-        minWidth: "250px"
-      }
-  });
+  background: "#f9f9eb",
+  border: 0,
+  borderRadius: 3,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  color: 'white',
+  height: "calc(98vh - 64px)",
+  paddingBottom: "60px",
+  top: "0",
+  bottom: "0",
+  left: "0",
+  right: "0",
+  margin: "auto",
+  '& .MuiTextField-root': {
+    margin: "10px",
+    width: "30%",
+    minWidth: "250px"
+  }
+});
 
 export default class HomePage extends Component {
     constructor(props) {
@@ -94,20 +98,39 @@ export default class HomePage extends Component {
               <Paper elevation={6} style={{minHeight: "100px"}}>
               <p>{this.state.reqs}</p>
               </Paper>
+              <Grid container direction="row" justify="space-evenly" alignItems="baseline">
+                <Grid item sm={5}>
+                  <Typography variant="h5" color="primary">Recommended Users</Typography>
+                  <Paper elevation={6} style={{minHeight: "100px"}}>
+                    <p></p>
+                  </Paper>
+                </Grid>
+                <Grid item sm={5}>
+                  <Typography variant="h5" color="primary">Similar Projects</Typography>
+                  <Paper elevation={6} style={{minHeight: "100px"}}>
+                    <p></p>
+                  </Paper>
+                </Grid>
+              </Grid>
             </Grid>
             <Grid item sm={3}>
-              <Paper>
-              <SvgIcon><svg width="24" height="24" viewBox="0 0 24 24"><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/></svg></SvgIcon>
-              Follow()
-              </Paper>
-              <Typography variant="h5" color="primary">Contributors</Typography>
-              <Paper elevation={6} style={{minHeight: "100px"}}>
-              {this.renderContributor()}
-              </Paper>
-              <Typography variant="h5" color="primary">Related Events</Typography>
-              <Paper elevation={6} style={{minHeight: "100px"}}>
-              {this.renderEvents()}
-              </Paper>
+              <Grid item sm={9}>
+                <Paper elevation={6} style={{padding:"5", width:"100%", background:"white", margin:"auto", marginBottom:"10px"}}>
+                <SvgIcon><svg width="24" height="24" viewBox="0 0 24 24"><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/></svg></SvgIcon>
+                Follow()
+                </Paper>
+              </Grid>
+              <Grid item sm={9} style={{ maxHeight:"40vh",minHeight: "20vh"}}>
+                <Typography variant="h5" color="primary">Contributors</Typography>
+                {this.renderContributor()}
+              </Grid>
+              <Grid item sm={9} style={{ maxHeight:"40vh",minHeight: "20vh"}}>
+                <Typography variant="h5" color="primary">Upcoming Events</Typography>
+                {this.renderEvents()}
+              </Grid>
+              <Grid item sm={9} style={{ maxHeight:"40vh",minHeight: "20vh"}}>
+                <Typography variant="h5" color="primary">Upcoming Deadlines</Typography>
+              </Grid>
             </Grid>
             </Grid>
           <CustomSnackbar ref={this.SnackbarRef} OpenSnackbar={this.handleSnackbarOpening} type={this.state.messageType} message={this.state.message}/>

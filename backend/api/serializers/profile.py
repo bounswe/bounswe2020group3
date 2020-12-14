b1a65c1ab2efc69bc34e0106fbf40eaae1b4db90
adilnumancelik <44122592+adilnumancelik@users.noreply.github.com>
2020-12-14 22:12:23 +0300

    Update profile.py

--- backend/api/serializers/profile.py
+++ backend/api/serializers/profile.py
@@ -59,4 +59,3 @@ class ProfilePrivateSerializer(serializers.HyperlinkedModelSerializer):
         model = Profile
         fields = ['id', 'name', 'middle_name', 'last_name',
                   'owner', 'profile_picture', 'is_public']
-

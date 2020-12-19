28189b9e5dd51aaf45498f72a90301007c2f0eb8
Furkan Cansever <furkancansever@Furkans-MacBook-Air.local>
2020-12-19 13:53:54 +0300

    users can access all their own milestones

--- backend/api/views/milestone.py
+++ backend/api/views/milestone.py
@@ -1,8 +1,15 @@
 from rest_framework import viewsets
 from rest_framework import permissions
 from api.models.milestone import Milestone
+from api.models.project import Project
+from api.serializers.project import ProjectGETPublicSerializer
+from rest_framework import generics
 from api.serializers.milestone import MilestoneSerializer
 from api.permission import IsMilestoneMemberOrReadOnly
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
 
 
 class MilestoneViewSet(viewsets.ModelViewSet):
@@ -14,3 +21,22 @@ class MilestoneViewSet(viewsets.ModelViewSet):
     serializer_class = MilestoneSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,
                           IsMilestoneMemberOrReadOnly]
+
+    @action(detail=False, methods=['GET'], url_name='get_user_milestone')
+    def get_user_milestone(self, request):
+        username = self.request.user.username
+        list_of_milestones = []
+        if username:
+            projects = Project.objects.filter(owner__username=username)
+            serializer = ProjectGETPublicSerializer(projects, many=True)
+            for item in serializer.data:
+                for milestone in item['milestones']:
+                    list_of_milestones.append(milestone)
+            list_of_milestones.sort(key=lambda x: x.get('date'))
+            return Response(data={
+                'result': list_of_milestones
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response(data={
+                'result': 'Not found'
+            }, status=status.HTTP_404_NOT_FOUND)

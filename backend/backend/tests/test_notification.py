9a14018b9c0a4464a2017021ca690a664df2edc0
Furkan Cansever <furkancansever@Furkans-MacBook-Air.local>
2020-12-22 00:58:37 +0300

    added test case

--- backend/backend/tests/test_notification.py
+++ backend/backend/tests/test_notification.py
@@ -4,6 +4,7 @@ from api.models.project import Project
 from notifications.signals import notify
 from rest_framework import status
 
+
 class NotificationTests(APITestCase):
     def setUp(self):
         self.owner = User.objects.create_user(
@@ -22,55 +23,25 @@ class NotificationTests(APITestCase):
             state='open for collaborators',
             owner=self.owner, due_date="2020-12-31")
 
-        self.draft_project = Project.objects.create(
-            name='Draft Project',
-            description='I just started this project',
-            state='draft',
-            owner=self.owner, due_date="2020-12-31")
+        self.client.force_authenticate(user=self.owner)
 
-        self.draft_project2 = Project.objects.create(
-            name='Draft Project',
-            description='I just started this project',
-            state='draft',
-            owner=self.owner, due_date="2021-03-31")
-
-        notify.send(sender=self.owner,
-                    recipient=self.invited_user,
-                    target=self.project,
-                    verb="invited to Project called {}".format(self.project.name),
-                    description="Invitation to Project")
-        notify.send(sender=self.owner,
-                    recipient=self.owner,
-                    target=self.draft_project,
-                    verb="created the new project called {}".format(self.draft_project.name),
-                    description="New Project")
-
-        notify.send(sender=self.owner,
-                    recipient=self.owner,
-                    target=self.draft_project2,
-                    verb="created the new project called {}".format(self.draft_project2.name),
-                    description="New Project")
+        self.client.post('/api/collaboration_invites/', {
+                "to_user": self.invited_user.id,
+                "to_project": self.project.id,
+                "message": "Come and join our project"
+            }, format='json')
 
     def test_can_get_own_notifications(self):
         self.client.force_authenticate(user=self.invited_user)
         response = self.client.get('/api/notifications/',
                                    format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        data = response.data[0]
+        data = response.data['result'][0]
         sender = data['actor']
         recipient = data['recipient']
-        target = data['target']
         verb = data['verb']
         self.assertEqual(sender['username'], self.owner.username)
         self.assertEqual(recipient['username'], self.invited_user.username)
-        self.assertEqual(target['state'], self.project.state)
-        self.assertEqual(verb, "invited to Project called {}".format(self.project.name))
-
-    def test_can_get_unread_notification(self):
-        self.client.force_authenticate(user=self.owner)
-        response = self.client.get('/api/notifications/unread/',
-                                   format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 2)
-
+        self.assertEqual(verb, "invited you to the Project {}".
+                         format(self.project.name))
 

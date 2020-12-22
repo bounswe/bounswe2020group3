1a37cd5183e81df5f54b739887ce07f54489a707
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-12-22 21:53:02 +0300

    FollowRequest: Create data class
    Signed-off-by: Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>

---
+++ android/app/src/main/java/com/bounswe2020group3/paperlayer/data/follow/FollowRequest.kt
@@ -0,0 +1,13 @@
+package com.bounswe2020group3.paperlayer.data.follow
+
+import com.bounswe2020group3.paperlayer.data.user.User
+import com.squareup.moshi.Json
+
+class FollowRequest (
+    @field:Json(name = "req_from_user")
+    var requestFromUser: User,
+    @field:Json(name = "req_to_user")
+    var requestToUser: User,
+    @field:Json(name = "created")
+    var created: String,
+) {}

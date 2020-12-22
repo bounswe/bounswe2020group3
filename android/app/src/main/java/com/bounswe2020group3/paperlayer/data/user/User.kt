130219538426c150624a39364e128125db0080b1
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-12-22 21:51:16 +0300

    User: Update data class
    Signed-off-by: Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>

--- android/app/src/main/java/com/bounswe2020group3/paperlayer/data/user/User.kt
+++ android/app/src/main/java/com/bounswe2020group3/paperlayer/data/user/User.kt
@@ -8,7 +8,13 @@ data class User (
     @field:Json(name = "username")
     var username: String,
     @field:Json(name = "email")
-    var email: String,
+    var email: String?,
     @field:Json(name = "profile")
     var profile: List<Profile>,
+    @field:Json(name = "is_follower")
+    var isFollower: Boolean,
+    @field:Json(name = "is_following")
+    var isFollowing: Boolean,
+    @field:Json(name = "is_follow_request_sent")
+    var isFollowRequestSent: Boolean,
 )

dcb2247af10598f6b5e026962ce3fef4b87a8406
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-12-22 23:42:08 +0300

    FollowContract.kt: Add additional follow, follow_request methods
    Signed-off-by: Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>

--- android/app/src/main/java/com/bounswe2020group3/paperlayer/profile/follow/FollowContract.kt
+++ android/app/src/main/java/com/bounswe2020group3/paperlayer/profile/follow/FollowContract.kt
@@ -1,7 +1,6 @@
 package com.bounswe2020group3.paperlayer.profile.follow
 
-import com.bounswe2020group3.paperlayer.data.follow.Follow
-import com.bounswe2020group3.paperlayer.data.follow.FollowType
+import com.bounswe2020group3.paperlayer.data.follow.*
 import com.bounswe2020group3.paperlayer.mvp.Mvp
 import io.reactivex.Observable
 import retrofit2.http.*
@@ -17,11 +16,22 @@ interface FollowContract {
     }
 
     interface Model {
+        fun sendFollow(userId: Int?, toUserId: Int): Observable<Any>
         fun getFollowerList(userId: Int?): Observable<List<Follow>>
         fun getFollowingList(userId: Int?): Observable<List<Follow>>
+        fun sendFollowRequest(userId: Int?, toUserId: Int): Observable<Any>
+        fun getFollowRequestList(userId: Int?): Observable<List<FollowRequest>>
+        fun acceptFollowRequest(followRequestId: Int): Observable<Any>
+        fun rejectFollowRequest(followRequestId: Int): Observable<Any>
     }
 
     interface Service {
+        @POST("/api/follow/")
+        fun sendFollow(
+            @Header("Authorization") authorization: String,
+            @Body followCreate: FollowCreate
+        ): Observable<Any>
+
         @GET("/api/follow/")
         fun getFollowerList(
             @Header("Authorization") authorization: String,
@@ -33,5 +43,29 @@ interface FollowContract {
             @Header("Authorization") authorization: String,
             @Query("from_user__id") userId: Int
         ): Observable<List<Follow>>
+
+        @POST("/api/follow_request/")
+        fun sendFollowRequest(
+            @Header("Authorization") authorization: String,
+            @Body followRequestCreate: FollowRequestCreate
+        ): Observable<Any>
+
+        @GET("/api/follow_request/")
+        fun getFollowRequestList(
+            @Header("Authorization") authorization: String,
+            @Query("req_to_user") userId: Int
+        ): Observable<List<FollowRequest>>
+
+        @POST("/api/follow_request/{id}/accept_follow/")
+        fun acceptFollowRequest(
+            @Header("Authorization") authorization: String,
+            @Path("id") followRequestId: Int
+        ): Observable<Any>
+
+        @POST("/api/follow_request/{id}/reject_follow/")
+        fun rejectFollowRequest(
+            @Header("Authorization") authorization: String,
+            @Path("id") followRequestId: Int
+        ): Observable<Any>
     }
 }

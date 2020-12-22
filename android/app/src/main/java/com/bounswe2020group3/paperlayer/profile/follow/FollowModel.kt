30dba6bcd88e31fecc887ea7e7b022f9e4afff59
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-12-22 23:43:53 +0300

    FollowModel.kt: Implement additional follow, follow request methods
    Signed-off-by: Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>

--- android/app/src/main/java/com/bounswe2020group3/paperlayer/profile/follow/FollowModel.kt
+++ android/app/src/main/java/com/bounswe2020group3/paperlayer/profile/follow/FollowModel.kt
@@ -1,6 +1,9 @@
 package com.bounswe2020group3.paperlayer.profile.follow
 
 import com.bounswe2020group3.paperlayer.data.follow.Follow
+import com.bounswe2020group3.paperlayer.data.follow.FollowCreate
+import com.bounswe2020group3.paperlayer.data.follow.FollowRequest
+import com.bounswe2020group3.paperlayer.data.follow.FollowRequestCreate
 import com.bounswe2020group3.paperlayer.data.user.AuthToken
 import com.bounswe2020group3.paperlayer.util.Session
 import io.reactivex.Observable
@@ -11,7 +14,7 @@ import java.lang.Exception
 import javax.inject.Inject
 
 class FollowModel @Inject constructor(
-    private val retrofit: Retrofit,
+    retrofit: Retrofit,
     private val sessionManager: Session
 ) : FollowContract.Model {
 
@@ -22,10 +25,27 @@ class FollowModel @Inject constructor(
         return sessionManager.getToken().value ?: throw Exception("No user found")
     }
 
-    override fun getFollowerList(userId: Int?): Observable<List<Follow>> {
+    private fun getSafeUserId(userId: Int?): Int {
         var safeUserId = userId
         if (safeUserId == null || safeUserId == 0)
             safeUserId = getAuthToken().id
+        return safeUserId
+    }
+
+    override fun sendFollow(userId: Int?, toUserId: Int): Observable<Any> {
+        val safeUserId = getSafeUserId(userId)
+        val followRequestCreate = FollowCreate(
+            fromUser = safeUserId,
+            toUser = toUserId
+        )
+        val authorization = "Token ${getAuthToken().token}"
+        return followService.sendFollow(authorization, followRequestCreate)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    override fun getFollowerList(userId: Int?): Observable<List<Follow>> {
+        val safeUserId = getSafeUserId(userId)
         val authorization = "Token ${getAuthToken().token}"
         return followService.getFollowerList(authorization, safeUserId)
             .subscribeOn(Schedulers.io())
@@ -33,11 +53,43 @@ class FollowModel @Inject constructor(
     }
 
     override fun getFollowingList(userId: Int?): Observable<List<Follow>> {
-        var safeUserId = userId
-        if (safeUserId == null || safeUserId == 0)
-            safeUserId = getAuthToken().id
+        val safeUserId = getSafeUserId(userId)
         val authorization = "Token ${getAuthToken().token}"
         return followService.getFollowingList(authorization, safeUserId)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    override fun sendFollowRequest(userId: Int?, toUserId: Int): Observable<Any> {
+        val safeUserId = getSafeUserId(userId)
+        val followRequestCreate = FollowRequestCreate(
+            requestFromUser = safeUserId,
+            requestToUser = toUserId
+        )
+        val authorization = "Token ${getAuthToken().token}"
+        return followService.sendFollowRequest(authorization, followRequestCreate)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    override fun getFollowRequestList(userId: Int?): Observable<List<FollowRequest>> {
+        val safeUserId = getSafeUserId(userId)
+        val authorization = "Token ${getAuthToken().token}"
+        return followService.getFollowRequestList(authorization, safeUserId)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    override fun acceptFollowRequest(followRequestId: Int): Observable<Any> {
+        val authorization = "Token ${getAuthToken().token}"
+        return followService.acceptFollowRequest(authorization, followRequestId)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    override fun rejectFollowRequest(followRequestId: Int): Observable<Any> {
+        val authorization = "Token ${getAuthToken().token}"
+        return followService.rejectFollowRequest(authorization, followRequestId)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
     }

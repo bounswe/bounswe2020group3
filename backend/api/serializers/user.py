6dbb99a53a91c942669d425fb636732aadc78e4c
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-12-22 18:40:34 +0300

    User serializers: Remove follow fields

--- backend/api/serializers/user.py
+++ backend/api/serializers/user.py
@@ -1,8 +1,5 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from api.serializers.following import FollowerSerializer,\
-    IncomingFollowRequestSerializer
-from api.serializers.following import FollowingSerializer
 
 from api.serializers.profile import ProfileFullSerializer, \
     ProfileBasicSerializer, ProfilePrivateSerializer
@@ -13,28 +10,21 @@ class UserFullSerializer(serializers.ModelSerializer):
     User serializer for admins.
     """
     profile = ProfileFullSerializer(many=True, read_only=True)
-    following = FollowerSerializer(many=True)
-    followers = FollowingSerializer(many=True)
-    follow_requests = IncomingFollowRequestSerializer(many=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'profile',
-                  'following', 'followers', 'follow_requests']
+        fields = ['id', 'username', 'email', 'profile']
 
 
 class UserBasicSerializer(serializers.ModelSerializer):
     """
     User serializer for users.
     """
-    following = FollowerSerializer(many=True)
-    followers = FollowingSerializer(many=True)
     profile = ProfileBasicSerializer(many=True, read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'profile',
-                  'following', 'followers']
+        fields = ['id', 'username', 'profile']
 
 
 class UserPrivateSerializer(serializers.ModelSerializer):

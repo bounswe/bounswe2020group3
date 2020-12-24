6b8acff62cb562b265ef2f0804f570e4e8689f5c
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-12-25 00:06:56 +0300

    Implement ListableFollow interface
    Signed-off-by: Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>

--- android/app/src/main/java/com/bounswe2020group3/paperlayer/data/follow/Follow.kt
+++ android/app/src/main/java/com/bounswe2020group3/paperlayer/data/follow/Follow.kt
@@ -4,10 +4,22 @@ import com.bounswe2020group3.paperlayer.data.user.User
 import com.squareup.moshi.Json
 
 class Follow(
-    @field:Json(name = "from_user")
-    var fromUser: User,
-    @field:Json(name = "to_user")
-    var toUser: User,
-    @field:Json(name = "created")
-    var created: String,
-) {}
+        @field:Json(name = "from_user")
+        var fromUser: User,
+        @field:Json(name = "to_user")
+        var toUser: User,
+        @field:Json(name = "created")
+        var created: String,
+) : ListableFollow {
+    override fun fetchFromUser(): User {
+        return fromUser
+    }
+
+    override fun fetchToUser(): User {
+        return toUser
+    }
+
+    override fun fetchCreated(): String {
+        return created
+    }
+}

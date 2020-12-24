da78f1167ae4e09ec694d90c96a4871e9f6d4181
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-12-24 12:00:31 +0300

    Implement dynamic toolbar title to follow list page
    Signed-off-by: Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>

--- android/app/src/main/java/com/bounswe2020group3/paperlayer/MainActivity.kt
+++ android/app/src/main/java/com/bounswe2020group3/paperlayer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.bounswe2020group3.paperlayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.navigation.NavController
@@ -11,6 +12,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.bounswe2020group3.paperlayer.dagger.AppComponent
 import com.bounswe2020group3.paperlayer.dagger.DaggerAppComponent
+import com.bounswe2020group3.paperlayer.data.follow.FollowType
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(){
@@ -29,15 +31,23 @@ class MainActivity : AppCompatActivity(){
         val appBarConfiguration = AppBarConfiguration(setOf(
             R.id.eventFragment, R.id.projectMainFragment, R.id.profileFragment,R.id.searchFragment
         ))
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            if(destination.id == R.id.followListFragment && arguments != null) {
+                if((arguments.get("followType") as FollowType) == FollowType.FOLLOWER) {
+                    destination.label = "Followers"
+                } else {
+                    destination.label = "Followings"
+                }
+            }
+        }
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
         navigationBarBottom.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        navController.navigateUp()
-
-        return super.onSupportNavigateUp()
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
     fun getAppComponent() = appComponent

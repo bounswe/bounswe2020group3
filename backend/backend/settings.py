bd16db679a5830a9bea7dbb3fc22f72877d551b7
adilnumancelik2017400198 <44122592+adilnumancelik2017400198@users.noreply.github.com>
2020-12-26 22:48:26 +0300

    AWS S3 setup for files
    -Set up AWS S3 for file and image uploads.
    
    -Profile pictures can be accessed using the url.
    -Files can not be accessed using the url due to security reasons. Only way to retrieve them is using the /file/retrieve_file endpoint.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -179,8 +179,8 @@ django_heroku.settings(locals())
 CORS_ORIGIN_ALLOW_ALL = True
 
 # AWS S3 for files
-AWS_ACCESS_KEY_ID = 'AKIAXJRLF4M4M62NMT2O'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = 'paperlayerbucket'
 AWS_S3_REGION_NAME = 'eu-central-1'
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,8 +17,6 @@ from django.urls import path, include
 from django.conf.urls import url
 from .swagger_api import schema_view
 from rest_framework.routers import DefaultRouter
-from django.conf import settings
-from django.conf.urls.static import static
 
 router = DefaultRouter()
 

944ca9ad846a2f4c6d9f50d0f483338eb514d8b7
rambo250 <ramazankoc5252@gmail.com>
2020-12-28 00:12:06 +0300

    Project Tags shown in search results. Search Fragment label updated. Project Settings and Invite Members hidden when user is not a owner. Search user id navigation updated. Some data classes updated according to API changes.

--- android/app/src/main/java/com/bounswe2020group3/paperlayer/project/ProjectDetailContract.kt
+++ android/app/src/main/java/com/bounswe2020group3/paperlayer/project/ProjectDetailContract.kt
@@ -1,8 +1,10 @@
 package com.bounswe2020group3.paperlayer.project
 
+import com.bounswe2020group3.paperlayer.data.user.AuthToken
 import com.bounswe2020group3.paperlayer.mvp.Mvp
 import com.bounswe2020group3.paperlayer.project.data.Project
 import io.reactivex.Single
+import io.reactivex.subjects.BehaviorSubject
 
 interface ProjectDetailContract {
 
@@ -13,6 +15,7 @@ interface ProjectDetailContract {
         fun setProject(project: Project)
         fun getProject(): Project?
         fun navigateToEditProject()
+        fun subscribeAuthToken()
     }
 
     interface View: Mvp.View{
@@ -25,10 +28,12 @@ interface ProjectDetailContract {
         fun resetMemberCardList()
         fun submitMemberCardList()
         fun addMemberCard(username: String)
+        fun updateCurrentUser(ownerID:Int)
     }
 
     interface Model {
         fun getProject(projectId: Int): Single<Project>
+        fun getAuthToken(): BehaviorSubject<AuthToken>
     }
 
 }

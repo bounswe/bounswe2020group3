331c758781ff4628a3c3ad380a6494005b4a68a9
mhrfky <44057069+mhrfky@users.noreply.github.com>
2021-01-24 20:55:27 +0300

    Error is handled
    Error is handled but not solved. I have exercised a temporary solution, adding try catch blocks.

--- android/app/src/main/java/com/bounswe2020group3/paperlayer/util/Constants.kt
+++ android/app/src/main/java/com/bounswe2020group3/paperlayer/util/Constants.kt
@@ -1,4 +1,4 @@
 package com.bounswe2020group3.paperlayer.util
 
-//const val BASE_URL = "https://paperlayer.herokuapp.com"
-const val BASE_URL = "https://paperlayer-backend.azurewebsites.net"
+const val BASE_URL = "https://paperlayer.herokuapp.com"
+//const val BASE_URL = "https://paperlayer-backend.azurewebsites.net"
